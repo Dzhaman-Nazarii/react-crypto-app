6a99fd99486bc234021f80551bb60316b21a315a
Nazarii <dzhaman.nazar2003@gmail.com>
2024-07-02 10:40:04 +0300

    improve prettier and add antd

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,10 @@
+import { DatePicker } from "antd";
+
 export default function App() {
-  return <h1>React Crypto App</h1>
+	return (
+		<>
+			<h1>React Crypto App</h1>
+			<DatePicker />
+		</>
+	);
 }
